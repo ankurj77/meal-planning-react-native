a4538d4b7409a240f0e6b3d9ad853d4514e7a051
Ankur Jain <ankurj.iitd@gmail.com>
2022-10-04 12:16:22 +0100

    detail page

--- screens/ItemScreen.js
+++ screens/ItemScreen.js
@@ -1,8 +1,9 @@
-import {Text, View} from "react-native";
+import {Image, Text, View, ScrollView, StyleSheet, FlatList} from "react-native";
 import {useRoute} from "@react-navigation/native";
 import {MEALS} from "../data/dummy-data";
+import {useLayoutEffect} from "react";
 
-function ItemScreen() {
+function ItemScreen({navigation}) {
   const route = useRoute();
   const itemID = route.params.itemID;
 
@@ -10,12 +11,78 @@ function ItemScreen() {
     return item.id === itemID;
   });
 
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: item.title,
+    });
+  }, [itemID, navigation])
+
   return (
-    <View>
-      <Text>{item.ingredients}</Text>
-      <Text>{item.steps}</Text>
-    </View>
+    <ScrollView horizontal={false} style={styles.innerContainer}>
+      <View>
+        <Image source={{uri: item.imageUrl}} style={styles.image} />
+        <Text style={styles.title}>{item.title}</Text>
+      </View>
+      <View style={styles.details}>
+        <Text style={styles.detailsItem}>Vegetarian: {item.isVegetarian ? "Yes" : "No"}</Text>
+        <Text style={styles.detailsItem}>Vegan: {item.isVegan ? "Yes" : "No"}</Text>
+        <Text style={styles.detailsItem}>Lactose Free: {item.isLactoseFree ? "Yes" : "No"}</Text>
+        <Text style={styles.detailsItem}>Gluten Free: {item.isGlutenFree ? "Yes" : "No"}</Text>
+        <Text style={styles.detailsItem}>Ingredients:</Text>
+        <FlatList
+          data={item.ingredients}
+          keyExtractor={(item) => item.id}
+          renderItem={(item) =>
+            <Text style={styles.detailsItemList}>{item.item}</Text>}
+        />
+        <Text style={styles.detailsItem}>Steps:</Text>
+        <FlatList
+          data={item.steps}
+          keyExtractor={(item) => item.id}
+          renderItem={(item) =>
+            <Text style={styles.detailsItemList}>{item.item}</Text>}
+        />
+      </View>
+    </ScrollView>
   );
 }
 
 export default ItemScreen;
+
+const styles = StyleSheet.create({
+  image: {
+    width: '100%',
+    height: 200,
+  },
+  title: {
+    color: 'white',
+    fontWeight: "bold",
+    textAlign: "center",
+    fontSize: 18,
+    margin: 8,
+  },
+  innerContainer: {
+    flex: 1,
+    marginBottom: 10,
+  },
+  details: {
+    flexDirection: "column",
+    alignItems: "baseline",
+    justifyContent: "center",
+    padding: 8,
+    marginBottom: 20,
+  },
+  detailsItem: {
+    marginHorizontal: 4,
+    fontSize: 12,
+    color: 'white',
+  },
+  detailsItemList: {
+    backgroundColor: '#ccc',
+    padding: 2,
+    marginHorizontal: 4,
+    marginVertical: 2,
+    color: 'black',
+    fontSize: 10,
+  }
+});
