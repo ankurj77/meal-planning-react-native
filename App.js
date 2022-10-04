0a4331958bb8add6291afe91afeea087ef73e61a
Ankur Jain <ankurj.iitd@gmail.com>
2022-10-04 17:41:29 +0100

    added a drawer navigation

--- App.js
+++ App.js
@@ -4,8 +4,18 @@ import {NavigationContainer} from "@react-navigation/native";
 import {createNativeStackNavigator} from "@react-navigation/native-stack";
 import ItemsScreen from "./screens/ItemsScreen";
 import ItemScreen from "./screens/ItemScreen";
+import {createDrawerNavigator} from "@react-navigation/drawer";
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+function DrawerNavigator() {
+  return (
+    <Drawer.Navigator>
+      <Drawer.Screen name="Categories" component={CategoriesScreen} />
+    </Drawer.Navigator>
+  );
+}
 
 export default function App() {
   return (
@@ -20,7 +30,7 @@ export default function App() {
             contentStyle: {backgroundColor: '#3f2f25'}
           }}
         >
-          <Stack.Screen name="Categories" component={CategoriesScreen} />
+          <Stack.Screen name="Drawer" component={DrawerNavigator} />
           <Stack.Screen name="Items" component={ItemsScreen} />
           <Stack.Screen name="Item" component={ItemScreen} />
         </Stack.Navigator>
