cbf97ad420438f0dd1380ee529a491b0ea78afa0
Ankur Jain <ankurj.iitd@gmail.com>
2022-10-05 09:00:50 +0100

    drawer with icons and styling

--- App.js
+++ App.js
@@ -5,14 +5,32 @@ import {createNativeStackNavigator} from "@react-navigation/native-stack";
 import ItemsScreen from "./screens/ItemsScreen";
 import ItemScreen from "./screens/ItemScreen";
 import {createDrawerNavigator} from "@react-navigation/drawer";
+import {Ionicons} from '@expo/vector-icons'
 
 const Stack = createNativeStackNavigator();
 const Drawer = createDrawerNavigator();
 
 function DrawerNavigator() {
   return (
-    <Drawer.Navigator>
-      <Drawer.Screen name="Categories" component={CategoriesScreen} />
+    <Drawer.Navigator
+      screenOptions={{
+        headerStyle: {backgroundColor: '#351401'},
+        headerTintColor: 'white',
+        sceneContainerStyle: {backgroundColor: '#3f2f25'},
+        drawerContentStyle: {backgroundColor: '#351401'},
+        drawerInactiveTintColor: "white",
+        drawerActiveTintColor: "#351401",
+        drawerActiveBackgroundColor: "#e4baa1",
+      }}
+    >
+      <Drawer.Screen
+        name="Categories"
+        component={CategoriesScreen}
+        options={{
+          title: "All Categories",
+          drawerIcon: ({color, size}) => (<Ionicons name="list" color={color} size={size} />)
+        }}
+      />
     </Drawer.Navigator>
   );
 }
@@ -30,7 +48,7 @@ export default function App() {
             contentStyle: {backgroundColor: '#3f2f25'}
           }}
         >
-          <Stack.Screen name="Drawer" component={DrawerNavigator} />
+          <Stack.Screen name="Drawer" component={DrawerNavigator} options={{headerShown: false}} />
           <Stack.Screen name="Items" component={ItemsScreen} />
           <Stack.Screen name="Item" component={ItemScreen} />
         </Stack.Navigator>
